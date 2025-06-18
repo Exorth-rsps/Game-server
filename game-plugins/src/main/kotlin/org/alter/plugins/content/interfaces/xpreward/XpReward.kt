d17380e157354ec83907c8860095e3906eb6dff1
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-18 22:01:12 +0200

    Fix xp reward confirm button

--- game-plugins/src/main/kotlin/org/alter/plugins/content/interfaces/xpreward/XpReward.kt
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/interfaces/xpreward/XpReward.kt
@@ -13,8 +13,10 @@ import org.alter.game.model.attr.XP_REWARD_SKILL
 
 object XpReward {
     const val INTERFACE_ID = 240
-    const val CONFIRM_TEXT = 26
-    const val CONFIRM_BUTTON = 27
+    /**
+     * The "Confirm" button component.
+     */
+    const val CONFIRM_COMPONENT = 26
 
     /**
      * Mapping of interface component ids to the skill they represent.
@@ -54,16 +56,15 @@ object XpReward {
         p.setInterfaceUnderlay(-1, -1)
         p.openInterface(INTERFACE_ID, InterfaceDestination.MAIN_SCREEN)
         p.setComponentText(INTERFACE_ID, 0, "Choose the stat you wish to be advanced!")
-        p.setComponentText(INTERFACE_ID, CONFIRM_TEXT, "Confirm")
+        p.setComponentText(INTERFACE_ID, CONFIRM_COMPONENT, "Confirm")
         // Initialize client-side script to handle button interactions and highlighting
         val containerHash = (INTERFACE_ID shl 16) or 1
-        val confirmHash = (INTERFACE_ID shl 16) or CONFIRM_BUTTON
+        val confirmHash = (INTERFACE_ID shl 16) or CONFIRM_COMPONENT
         p.runClientScript(3804, containerHash, confirmHash)
         COMPONENT_TO_SKILL.keys.forEach { comp ->
             p.setInterfaceEvents(INTERFACE_ID, comp, 1..1, InterfaceEvent.ClickOp1)
         }
         p.setInterfaceEvents(INTERFACE_ID, 1, 1..1, InterfaceEvent.ClickOp1)
-        p.setInterfaceEvents(INTERFACE_ID, CONFIRM_BUTTON, 1..1, InterfaceEvent.ClickOp1)
-        p.setInterfaceEvents(INTERFACE_ID, CONFIRM_TEXT, 1..1, InterfaceEvent.ClickOp1)
+        p.setInterfaceEvents(INTERFACE_ID, CONFIRM_COMPONENT, 1..1, InterfaceEvent.ClickOp1)
     }
 }
