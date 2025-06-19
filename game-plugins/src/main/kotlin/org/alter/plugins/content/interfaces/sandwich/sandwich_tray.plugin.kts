9d4ac5109cfca106a7d565fd10af04fb20ca96a1
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-19 22:42:56 +0200

    Fix sandwich tray plugin

--- game-plugins/src/main/kotlin/org/alter/plugins/content/interfaces/sandwich/sandwich_tray.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/interfaces/sandwich/sandwich_tray.plugin.kts
@@ -1,9 +1,10 @@
 package org.alter.plugins.content.interfaces.sandwich
 
 import org.alter.game.message.impl.ResumePauseButtonMessage
+import org.alter.game.model.queue.submitReturnValue
 
 SandwichTray.itemComponents.forEach { component ->
     on_button(interfaceId = SandwichTray.INTERFACE_ID, component = component) {
-        player.queues.submitReturnValue(ResumePauseButtonMessage(SandwichTray.INTERFACE_ID, component, -1))
+        player.submitReturnValue(ResumePauseButtonMessage(SandwichTray.INTERFACE_ID, component, -1))
     }
 }
