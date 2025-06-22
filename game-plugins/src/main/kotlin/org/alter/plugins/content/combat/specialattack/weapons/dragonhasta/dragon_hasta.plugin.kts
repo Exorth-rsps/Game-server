bd5335b4d0682dcddebd0de382af70a64d6f3773
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-22 19:01:27 +0200

    Add Dragon Hasta special attack

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/combat/specialattack/weapons/dragonhasta/dragon_hasta.plugin.kts
@@ -0,0 +1,43 @@
+package org.alter.plugins.content.combat.specialattack.weapons.dragonhasta
+
+import org.alter.game.model.entity.AreaSound
+import org.alter.api.cfg.Animation
+import org.alter.api.cfg.Graphic
+import org.alter.api.cfg.Items
+import org.alter.api.cfg.Sound
+import org.alter.plugins.content.combat.dealHit
+import org.alter.plugins.content.combat.formula.MeleeCombatFormula
+import org.alter.plugins.content.combat.specialattack.SpecialAttacks
+
+val SPECIAL_REQUIREMENT = 30
+
+private fun executeSpec() {
+    player.animate(id = Animation.DRAGON_HASTA_SPECIAL)
+    player.graphic(id = Graphic.DRAGON_HASTA_SPECIAL)
+    world.spawn(AreaSound(tile = player.tile, id = Sound.PUNCTURE, radius = 10, volume = 1))
+
+    val maxHit = MeleeCombatFormula.getMaxHit(player, target, specialAttackMultiplier = 1.05)
+    val accuracy = MeleeCombatFormula.getAccuracy(player, target, specialAttackMultiplier = 1.05)
+    val landHit = accuracy >= world.randomDouble()
+    player.dealHit(target = target, maxHit = maxHit, landHit = landHit, delay = 1)
+}
+
+SpecialAttacks.register(Items.DRAGON_HASTA, SPECIAL_REQUIREMENT) {
+    executeSpec()
+}
+
+SpecialAttacks.register(Items.DRAGON_HASTAP, SPECIAL_REQUIREMENT) {
+    executeSpec()
+}
+
+SpecialAttacks.register(Items.DRAGON_HASTAP_22737, SPECIAL_REQUIREMENT) {
+    executeSpec()
+}
+
+SpecialAttacks.register(Items.DRAGON_HASTAP_22740, SPECIAL_REQUIREMENT) {
+    executeSpec()
+}
+
+SpecialAttacks.register(Items.DRAGON_HASTAKP, SPECIAL_REQUIREMENT) {
+    executeSpec()
+}
