1fb6d16b6020c1cb2da88beea6cbe68653ad33a3
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-22 19:55:57 +0200

    Rewrite Dragon Hasta special attack

--- game-plugins/src/main/kotlin/org/alter/plugins/content/combat/specialattack/weapons/dragonhasta/dragon_hasta.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/combat/specialattack/weapons/dragonhasta/dragon_hasta.plugin.kts
@@ -11,28 +11,57 @@ import org.alter.plugins.content.combat.specialattack.SpecialAttacks
 
 val SPECIAL_REQUIREMENT = 30
 
-private fun register(item: Int) {
-    SpecialAttacks.register(item, SPECIAL_REQUIREMENT) {
-        player.animate(id = Animation.DRAGON_HASTA_SPECIAL)
-        player.graphic(id = Graphic.DRAGON_HASTA_SPECIAL)
-        world.spawn(
-            AreaSound(
-                tile = player.tile,
-                id = Sound.PUNCTURE,
-                radius = 10,
-                volume = 1
-            )
-        )
+SpecialAttacks.register(Items.DRAGON_HASTA, SPECIAL_REQUIREMENT) {
+    player.animate(id = Animation.DRAGON_HASTA_SPECIAL)
+    player.graphic(id = Graphic.DRAGON_HASTA_SPECIAL)
+    world.spawn(AreaSound(tile = player.tile, id = Sound.PUNCTURE, radius = 10, volume = 1))
 
-        val maxHit = MeleeCombatFormula.getMaxHit(player, target, specialAttackMultiplier = 1.05)
-        val accuracy = MeleeCombatFormula.getAccuracy(player, target, specialAttackMultiplier = 1.05)
-        val landHit = accuracy >= world.randomDouble()
-        player.dealHit(target = target, maxHit = maxHit, landHit = landHit, delay = 1)
-    }
+    val maxHit = MeleeCombatFormula.getMaxHit(player, target, specialAttackMultiplier = 1.05)
+    val accuracy = MeleeCombatFormula.getAccuracy(player, target, specialAttackMultiplier = 1.05)
+    val landHit = accuracy >= world.randomDouble()
+    player.dealHit(target = target, maxHit = maxHit, landHit = landHit, delay = 1)
 }
 
-register(Items.DRAGON_HASTA)
-register(Items.DRAGON_HASTAP)
-register(Items.DRAGON_HASTAP_22737)
-register(Items.DRAGON_HASTAP_22740)
-register(Items.DRAGON_HASTAKP)
+SpecialAttacks.register(Items.DRAGON_HASTAP, SPECIAL_REQUIREMENT) {
+    player.animate(id = Animation.DRAGON_HASTA_SPECIAL)
+    player.graphic(id = Graphic.DRAGON_HASTA_SPECIAL)
+    world.spawn(AreaSound(tile = player.tile, id = Sound.PUNCTURE, radius = 10, volume = 1))
+
+    val maxHit = MeleeCombatFormula.getMaxHit(player, target, specialAttackMultiplier = 1.05)
+    val accuracy = MeleeCombatFormula.getAccuracy(player, target, specialAttackMultiplier = 1.05)
+    val landHit = accuracy >= world.randomDouble()
+    player.dealHit(target = target, maxHit = maxHit, landHit = landHit, delay = 1)
+}
+
+SpecialAttacks.register(Items.DRAGON_HASTAP_22737, SPECIAL_REQUIREMENT) {
+    player.animate(id = Animation.DRAGON_HASTA_SPECIAL)
+    player.graphic(id = Graphic.DRAGON_HASTA_SPECIAL)
+    world.spawn(AreaSound(tile = player.tile, id = Sound.PUNCTURE, radius = 10, volume = 1))
+
+    val maxHit = MeleeCombatFormula.getMaxHit(player, target, specialAttackMultiplier = 1.05)
+    val accuracy = MeleeCombatFormula.getAccuracy(player, target, specialAttackMultiplier = 1.05)
+    val landHit = accuracy >= world.randomDouble()
+    player.dealHit(target = target, maxHit = maxHit, landHit = landHit, delay = 1)
+}
+
+SpecialAttacks.register(Items.DRAGON_HASTAP_22740, SPECIAL_REQUIREMENT) {
+    player.animate(id = Animation.DRAGON_HASTA_SPECIAL)
+    player.graphic(id = Graphic.DRAGON_HASTA_SPECIAL)
+    world.spawn(AreaSound(tile = player.tile, id = Sound.PUNCTURE, radius = 10, volume = 1))
+
+    val maxHit = MeleeCombatFormula.getMaxHit(player, target, specialAttackMultiplier = 1.05)
+    val accuracy = MeleeCombatFormula.getAccuracy(player, target, specialAttackMultiplier = 1.05)
+    val landHit = accuracy >= world.randomDouble()
+    player.dealHit(target = target, maxHit = maxHit, landHit = landHit, delay = 1)
+}
+
+SpecialAttacks.register(Items.DRAGON_HASTAKP, SPECIAL_REQUIREMENT) {
+    player.animate(id = Animation.DRAGON_HASTA_SPECIAL)
+    player.graphic(id = Graphic.DRAGON_HASTA_SPECIAL)
+    world.spawn(AreaSound(tile = player.tile, id = Sound.PUNCTURE, radius = 10, volume = 1))
+
+    val maxHit = MeleeCombatFormula.getMaxHit(player, target, specialAttackMultiplier = 1.05)
+    val accuracy = MeleeCombatFormula.getAccuracy(player, target, specialAttackMultiplier = 1.05)
+    val landHit = accuracy >= world.randomDouble()
+    player.dealHit(target = target, maxHit = maxHit, landHit = landHit, delay = 1)
+}
