d7559a7f1f16399f7cd72876699bd4d74e1f1929
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-27 20:00:02 +0200

    Update magic_longbow.plugin.kts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/combat/specialattack/weapons/magiclongbow/magic_longbow.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/combat/specialattack/weapons/magiclongbow/magic_longbow.plugin.kts
@@ -2,6 +2,7 @@ package org.alter.plugins.content.combat.specialattack.weapons.magiclongbow
 
 import org.alter.game.model.entity.AreaSound
 import org.alter.api.cfg.Animation
+import org.alter.api.cfg.Graphic
 import org.alter.api.cfg.Items
 import org.alter.api.cfg.Sound
 import org.alter.plugins.content.combat.dealHit
@@ -12,6 +13,7 @@ val SPECIAL_REQUIREMENT = 55
 
 SpecialAttacks.register(Items.MAGIC_LONGBOW, SPECIAL_REQUIREMENT) {
     player.animate(id = Animation.HUMAN_BOW_ATTACK)
+    player.graphic(id = Graphic.CRYSTAL_BOW_DRAWBACK)
     world.spawn(AreaSound(tile = player.tile, id = Sound.LONGBOW, radius = 10, volume = 1))
 
     val maxHit = RangedCombatFormula.getMaxHit(player, target, specialAttackMultiplier = 1.5)
