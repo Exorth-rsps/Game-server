4d17d59ec483ca21020ea72e771ecfa9599fe5cb
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-08-26 20:50:27 +0200

    Add autocast spell selection interface

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/interfaces/gameframe/tabs/magic/autocast.plugin.kts
@@ -0,0 +1,39 @@
+package org.alter.plugins.content.interfaces.autocast
+
+import org.alter.game.model.interf.InterfaceDestination
+import org.alter.plugins.content.combat.Combat
+import org.alter.plugins.content.combat.autocast.Autocast
+import org.alter.plugins.content.combat.autocast.AutocastSpells
+import org.alter.plugins.content.interfaces.attack.AttackTab.ATTACK_TAB_INTERFACE_ID
+
+private const val AUTOCAST_INTERFACE_ID = 201
+private const val SPELL_COMPONENT = 1
+private const val DEFENSIVE_COMPONENT = 3
+private const val CLOSE_COMPONENT = 5
+
+on_button(interfaceId = ATTACK_TAB_INTERFACE_ID, component = 23) {
+    player.openInterface(interfaceId = AUTOCAST_INTERFACE_ID, dest = InterfaceDestination.TAB_AREA)
+    player.setInterfaceEvents(interfaceId = AUTOCAST_INTERFACE_ID, component = SPELL_COMPONENT, range = 0..50, setting = 2)
+}
+
+on_button(interfaceId = AUTOCAST_INTERFACE_ID, component = SPELL_COMPONENT) {
+    val slot = player.getInteractingSlot()
+    val spell = AutocastSpells.forId(slot + 1) ?: return@on_button
+    if (Autocast.canAutocast(player, spell)) {
+        player.attr[Combat.CASTING_SPELL] = spell
+        player.setVarbit(Combat.SELECTED_AUTOCAST_VARBIT, spell.autoCastId)
+        player.closeInterface(AUTOCAST_INTERFACE_ID)
+    } else {
+        player.message("You can't autocast that spell with this staff.")
+        Autocast.reset(player)
+    }
+}
+
+on_button(interfaceId = AUTOCAST_INTERFACE_ID, component = DEFENSIVE_COMPONENT) {
+    player.toggleVarbit(Combat.DEFENSIVE_MAGIC_CAST_VARBIT)
+}
+
+on_button(interfaceId = AUTOCAST_INTERFACE_ID, component = CLOSE_COMPONENT) {
+    player.closeInterface(AUTOCAST_INTERFACE_ID)
+}
+
